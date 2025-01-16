39f732c3d91dc92399e0fd8c343b96817809e555
dinogomez <dinogomez117@gmail.com>
2025-01-16 17:42:15 +0800

    update 2025

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,11 +1,11 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-export function getYearsOfExperience(yearStart:number) {
+export function getYearsOfExperience(yearStart: number) {
   const currentYear = new Date().getFullYear();
-  return currentYear - yearStart;
+  return Math.round(currentYear - yearStart);
 }
